031ecfbbc3a2ebc6cd37f0dd084b78cf5a16e3ee
Briamco <crossinerobriamco@gmail.com>
2025-07-02 13:49:35 -0400

    feat(client): react-router configuration

---
+++ client/src/Pages/ScreenView.tsx
@@ -0,0 +1,10 @@
+function ScreenView() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Screen</h1>
+      <p className="text-lg text-gray-700">Welcome to your Screen!</p>
+    </div>
+  );
+}
+
+export default ScreenView;
